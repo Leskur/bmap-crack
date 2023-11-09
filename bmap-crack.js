b748499f13f493059909c3056e0972594f9ee8c6
Leskur <33910980+Leskur@users.noreply.github.com>
2023-11-09 15:34:13 +0800

    Update bmap-crack.js

--- bmap-crack.js
+++ bmap-crack.js
@@ -1,24 +1,25 @@
 (function () {
   let BMapGL = window.BMapGL;
-
+  window.__BMapCracked = false;
   const _rd = new Proxy(
     {},
     {
       set: function (target, prop, value) {
-        if (
-          /function\s?\(\w+\){\w+&&\w+\(\w+\);delete BMapGL._rd/i.test(
-            value.toString()
-          )
-        ) {
-          return false;
+        if (window.__BMapCracked) {
+          return Reflect.set(target, prop, value);
         }
-        return Reflect.set(target, prop, value);
+        if (/未获得百度地图商用授权/.test(value.toString())) {
+          window.__BMapCracked = true;
+        }
+        return false;
       },
-    }
+    },
   );
+  
   if (BMapGL) {
     BMapGL._rd = _rd;
   }
+  
   Object.defineProperty(window, "BMapGL", {
     get() {
       return BMapGL;
