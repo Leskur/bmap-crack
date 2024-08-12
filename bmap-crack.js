75936cb60c44e3f22a2b3b8ddbc375060f8cb9e5
Leskur <leskur@chinafuturelink.org>
2024-08-12 12:48:56 +0800

    兼容 BMapGL 和 BMap

--- bmap-crack.js
+++ bmap-crack.js
@@ -1,36 +1,28 @@
 (function () {
-  let BMapGL = window.BMapGL;
-  window.__BMapCracked = false;
-  const _rd = new Proxy(
-    {},
-    {
-      set: function (target, prop, value) {
-        if (window.__BMapCracked) {
-          return Reflect.set(target, prop, value);
-        }
-        if (/未获得百度地图商用授权/.test(value.toString())) {
-          window.__BMapCracked = true;
-        }
+  let BMap = window.BMapGL || window.BMap;
+
+  const _rd = new Proxy({}, {
+    set: function (target, prop, value) {
+      if (/_cbk/.test(prop)) {
         return false;
-      },
-    },
-  );
-  
-  if (BMapGL) {
-    BMapGL._rd = _rd;
-  }
-  
-  Object.defineProperty(window, "BMapGL", {
-    get() {
-      return BMapGL;
-    },
-    set(value) {
-      BMapGL = value;
-      if (!BMapGL._rd) {
-        Object.defineProperty(BMapGL, "_rd", {
-          value: _rd,
-        });
       }
+      return Reflect.set(target, prop, value);
     },
+  });
+  if (BMap) {
+    BMap._rd = _rd;
+  }
+  const descriptor = {
+    get() {
+      return BMap;
+    }, set(value) {
+      BMap = value;
+      Object.defineProperty(BMap, "_rd", {
+        value: _rd,
+      });
+    },
+  }
+  Object.defineProperties(window, {
+    "BMap": descriptor, "BMapGL": descriptor,
   });
 })();
